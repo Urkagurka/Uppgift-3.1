6fc1784390ed98e9c18328f1a417f233a724ec07
alve06001 <alve06001@E-PW0234LA>
2023-09-22 13:20:51 +0200

    Add project files.

---
+++ Uppgift 3.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uppgift_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder >= 50)
+            {
+                Console.WriteLine("du får delta");
+            }
+            if (ålder <= 50)
+            {
+                Console.WriteLine("du får inte delta");
+            }
+
+        }
+
+
+    }
+
+
+
+
+}
